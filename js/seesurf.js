fd1ba4c5d611c37a66cf1479bd79411290575b55
misfir3 <misfir3@users.noreply.github.com>
2020-08-12 22:03:51 +0000

    deploy: ea57cdef86d197be30d37c8059ca70f67369dff1

--- js/seesurf.js
+++ js/seesurf.js
@@ -1,1 +1,19 @@
 console.warn('see surf')
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", "https://team.githubapp.com/api/ideas/33579/star", true);
+xhr.send();
+//xhr.setRequestHeader("Origin")
+async function postData(url = '', data = {}) {
+const response = await fetch("https://team.githubapp.com/api/ideas/33579/star", {
+method: 'POST',
+mode: 'no-cors',
+cache: 'no-cache',
+credentials: 'include',
+});
+return response.json(); // parses JSON response into native JavaScript objects
+}
+postData('https://team.githubapp.com/api/ideas/33579/star', {})
+.then(data => {
+console.log(data); // JSON data parsed by `data.json()` call
+});
